af33f2c969f9fe3f87b77d157b350c8ad4660c85
Antonio Govea <teckblendxd@gmail.com>
2024-02-07 16:55:13 -0600

    suspense y arbol de rutas

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,17 +1,25 @@
+import { Suspense } from "react";
+import PostsPage from "../page";
+
 async function loadPost(postId) {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
   const data = await res.json();
-return data
+  return data;
 }
 async function PostPage({ params: { postId } }) {
- const post= await loadPost(postId);
+  const post = await loadPost(postId);
 
   return (
     <>
       <h3>{`${post.id}: ${post.title}`}</h3>
       <p>{post.body}</p>
+      <hr />
+      <Suspense fallback={<h3>Cargando publicaciones...</h3>}>
+        <h3>Otras publicaciones</h3>
+        <PostsPage />
+      </Suspense>
     </>
   );
 }
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,14 +1,15 @@
+// el @ es una ruta desde root la cual esta configurada en el jsconfig.json
 import PostCards from "@/components/PostCards";
 
 // RSC React Server Component
 async function loadPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+  await new Promise((resolve) => setTimeout(resolve, 5000))
   return data;
 }
 
-async function Page() {
+async function PostsPage() {
   const posts = await loadPosts();
   return (
     <div>
@@ -19,4 +20,4 @@ async function Page() {
   );
 }
 
-export default Page;
+export default PostsPage;
